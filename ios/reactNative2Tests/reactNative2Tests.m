eea25dccc4c143541145ad4d45ee1892b3cc2f70
HieuTQ <truongquanghieu8198@gmail.com>
2020-04-17 16:47:00 +0700

    Update setup second-times

--- ios/reactNative2Tests/reactNative2Tests.m
+++ ios/reactNative2Tests/reactNative2Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface threeTests : XCTestCase
+@interface reactNative2Tests : XCTestCase
 
 @end
 
-@implementation threeTests
+@implementation reactNative2Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {

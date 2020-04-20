9a82cac181f8791272f216136a3794b881fb1362
HieuTQ <truongquanghieu8198@gmail.com>
2020-04-20 16:47:23 +0700

    Init FlatList, Using flex in listItem include image and test, Implement Swipeout to delete element in list

---
+++ components/BasicFlatList.js
@@ -0,0 +1,141 @@
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View, Image, Alert, Platform, TouchableHighlight } from 'react-native';
+import flatListData from '../data/flatListData';
+import Swipeout from 'react-native-swipeout';
+import ButtonAdd from '../assets/images/icons-add.png';
+
+const FlatListItem = ({item, index, refreshFlatList}) => {
+    const [activeRowKey, setActiveRowKey] = useState(null);
+
+    const swipeSettings = {
+        autoClose: true,
+        // swipe right to left
+        onClose: (secId, rowId, direction) => {
+            if(!activeRowKey === null) {
+                setActiveRowKey(null)
+            }
+        },
+        // swipe left to right
+        onOpen: (secId, rowId, direction) => {
+            setActiveRowKey(item.key)
+        },
+        // setting button on right
+        right: [{
+            onPress: () => {
+                Alert.alert(
+                    'Alert',
+                    'Are you sure want to delete ?',
+                    [
+                        {
+                            text: 'No',
+                            onPress: () => console.log('Cancel Pressed'),
+                            style: 'cancel'
+                        },
+                        {
+                            text: 'Yes',
+                            onPress: () => {
+                                flatListData.splice(index, 1);
+                                //Refresh FlatList !
+                                refreshFlatList(activeRowKey);
+                            }
+                        }
+                    ],
+                    { cancelable: true }
+                )
+            },
+            text: 'Delete',
+            type: 'delete'
+        }],
+        rowId: index,
+        sectionId: 1
+    };
+
+    return (
+        <Swipeout {...swipeSettings}>
+            <View style={{
+                flex: 1,
+                flexDirection: 'column'
+            }}>
+                <View style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    backgroundColor: 'mediumseagreen',
+                    // backgroundColor: index % 2 === 0 ? 'mediumseagreen': 'tomato'
+                }}>
+                    <Image
+                        source={{ uri: item.imageUrl }}
+                        style={{ width: 100, height: 100, margin: 5 }}
+                    />
+                    <View style={{
+                        flex: 1,
+                        flexDirection: 'column'
+                    }}>
+                        <Text style={styles.flatListItem}>{item.name}</Text>
+                        <Text style={styles.flatListItem}>{item.foodDescription}</Text>
+                    </View>
+                </View>
+
+                <View style={{
+                    height: 1,
+                    backgroundColor:'white'
+                }}>
+                </View>
+            </View>
+        </Swipeout>
+    )
+}
+
+const styles = StyleSheet.create({
+    flatListItem: {
+        color: 'white',
+        padding: 10,
+        fontSize: 16,
+    }
+})
+
+export default function BasicFlatList() {
+    const [deletedRowKey, setDeletedRowKey] = useState(null);
+
+    function refreshFlatList(deletedKey) {
+        setDeletedRowKey(deletedKey);
+    }
+
+    function _onPressAdd () {
+        alert("You add Item");
+    }
+
+    return (
+        <View style={{ flex:1 , marginTop: Platform.OS === 'ios' ? 34 : 0 }}>
+            <View style={{
+                backgroundColor: 'tomato',
+                flexDirection: 'row',
+                justifyContent:'flex-end',
+                alignItems: 'center',
+                height: 55
+            }}>
+                <TouchableHighlight
+                    style={{marginRight: 10}}
+                    underlayColor='tomato'
+                    onPress={_onPressAdd}
+                >
+                    <Image
+                        style={{width: 35, height: 35}}
+                        source={ButtonAdd}
+                    />
+                </TouchableHighlight>
+            </View>
+            <FlatList
+                data={flatListData}
+                renderItem={({item, index}) => {
+                    // console.log(`Item = ${JSON.stringify(item)}, Index = ${index}`)
+                    return (
+                        <FlatListItem item={item} index={index} refreshFlatList={refreshFlatList}>
+
+                        </FlatListItem>
+                    )
+                }}
+            >
+            </FlatList>
+        </View>
+    );
+}
